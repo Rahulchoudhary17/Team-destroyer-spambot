6710e0b11a38d344235c057f0b26a2c458feb46a
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-09 22:49:24 +0530

    Update string.sh

--- string.sh
+++ string.sh
@@ -1,1 +1,1 @@
-pkg update && pkg upgrade && cd $RK && rm -rf string_session.py && apt install -y git wget python && wget https://raw.githubusercontent.com/YukkiBot/YukkiSpamBot/master/string_session.py && pip install telethon && clear && python string_session.py
+pkg update && pkg upgrade && cd $RK && rm -rf string_session.py && apt install -y git wget python && wget https://raw.githubusercontent.com/Rahulchoudhary17/Team-destroyer-spambot/master/string_session.py && pip install telethon && clear && python string_session.py
